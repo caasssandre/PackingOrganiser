ac51929abf6d419e13be66fa1cdf16cd0b745a8d
Cassandre-Guinut <guinutcassandre@gmail.com>
2019-10-27 15:56:58 +1300

    change findTrip to findObject

--- tripsRoutes.js
+++ tripsRoutes.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router()
 const data = require('./data.json')
 const functions = require('./functions')
+const activitiesData = require('./activitiesData.json')
 
 router.get('/past', (req, res)=>{
   const viewData = {}
@@ -19,7 +20,11 @@ router.get('/future', (req, res)=>{
 })
 
 router.get('/future/:id', (req, res)=>{
-  const viewData = functions.findTrip(data, req.params.id)
+  const trip = functions.findObject(data, 'id', req.params.id)
+  //console.log(trip.activity)
+  const viewData = functions.findObject(activitiesData, 'activity', trip.activity)
+  //viewData[destination] = trip.destination
+  //viewData[date] = trip.date
   res.render('./trips/future_trip', viewData)
 })
 
