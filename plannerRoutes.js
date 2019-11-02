5e8d65c7cffe9ca27b26cb12fcf535644de0e2bb
Cassandre-Guinut <guinutcassandre@gmail.com>
2019-11-02 18:09:16 +1300

    change addTrip to add to databse

--- plannerRoutes.js
+++ plannerRoutes.js
@@ -1,18 +1,24 @@
 const express = require("express")
 const router = express.Router()
-const functions = require('./functions')
 const dbMethods = require('./dbMethods')
 
 
 router.get('/', (req, res)=>{
-  const viewData = {items : activitiesData}
-  res.render('./trips/planner', viewData)
+  // const viewData = {items : activitiesData}
+  res.render('./trips/planner', {})
 })
 
 router.post('/', (request, response) => {
-  let id = data.length + 1
-  response.redirect('/trips/future/' + id)
-  functions.addTrip(id, request, data)
+  let newTrip = {
+    destination : request.body.destination,
+    trip_date : request.body.trip_date,
+    party_size : request.body.trip_date
+  }
+  console.log(newTrip)
+  dbMethods.addTrip(newTrip).then(thisTrip=>{
+    // use thisTrip.trip_id to redirect to correct page
+    response.redirect('/trips/future/')
+  })
 })
 
 module.exports = router
